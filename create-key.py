769096b7ec533003c4f7fc205a74e8befdc265f0
evilwizardington <43245597+evilwizardington@users.noreply.github.com>
2020-05-13 23:04:17 -0500

    Create create-key.py

---
+++ create-key.py
@@ -0,0 +1,15 @@
+import base64
+import os
+
+def generate_encryption_key():
+    """Generates a 256 bit (32 byte) AES encryption key and prints the
+    base64 representation.
+    This is included for demonstration purposes. You should generate your own
+    key. Please remember that encryption keys should be handled with a
+    comprehensive security policy.
+    """
+    key = os.urandom(32)
+    encoded_key = base64.b64encode(key).decode("utf-8")
+    print("Base 64 encoded encryption key: {}".format(encoded_key))
+    
+generate_encryption_key()
